02b761190844becfff7f97022c5a0978e0bd361d
luannt1010 <luanntai13@gmail.com>
2026-07-24 22:37:03 +0700

    add update code model

--- embedded_model/src/net.py
+++ embedded_model/src/net.py
@@ -4,7 +4,7 @@ try:
 except ModuleNotFoundError:
     from iresnet import iresnet18, iresnet34, iresnet50, iresnet100, iresnet200
 
-class ResNetEncoder(nn.Module):
+class IResNetEncoder(nn.Module):
     def __init__(self, model_size=18, embedding_dim=512, dropout=0.4):
         super().__init__()
         self.model_size = model_size
@@ -30,6 +30,58 @@ class ResNetEncoder(nn.Module):
 
     def forward(self, x):
         x = self.backbone(x)
+        return x
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, skip_connection=False):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+                                   nn.BatchNorm2d(out_channels))
+        self.conv2 = nn.Sequential(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+                                   nn.BatchNorm2d(out_channels))
+        self.relu = nn.ReLU()
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.skip_connection = skip_connection
+        if self.skip_connection:
+            self.shortcut = self.make_shortcut(in_channels, out_channels) if in_channels != out_channels else nn.Identity()
+    
+    def make_shortcut(self, in_channels, out_channels):
+        return nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+                             nn.BatchNorm2d(out_channels))
+        
+    def forward(self, x):
+        identity = x
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        if self.skip_connection:
+            x += self.shortcut(identity)
+        x = self.relu(x)
+        x = self.max_pool(x)
+        return x 
+
+
+class MyModel(nn.Module):
+    def __init__(self, embedding_dim=512, dropout=0.4):
+        super().__init__()
+        self.block1 = CNNBlock(in_channels=3, out_channels=32, skip_connection=True)
+        self.block2 = CNNBlock(in_channels=32, out_channels=64, skip_connection=True)
+        self.block3 = CNNBlock(in_channels=64, out_channels=128, skip_connection=True)
+        self.block4 = CNNBlock(in_channels=128, out_channels=256, skip_connection=True)
+        self.dropout = dropout
+        self.embedding_dim = embedding_dim
+        self.extract_embed = nn.Sequential(nn.Flatten(), 
+                                        nn.Linear(256*7*7, 1024), nn.ReLU(), nn.Dropout(self.dropout), 
+                                        nn.Linear(1024, 512), nn.ReLU(), nn.Dropout(self.dropout), 
+                                        nn.Linear(512, self.embedding_dim))
+    
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.extract_embed(x)
         return x
 
 
